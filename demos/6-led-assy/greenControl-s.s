30059f29c297ecc78e99e865fb624ac41f825396
bmen2a <bmendoza7@miners.utep.edu>
2022-10-18 12:43:13 -0600

    comment to better understand assembly

--- demos/6-led-assy/greenControl-s.s
+++ demos/6-led-assy/greenControl-s.s
@@ -3,12 +3,12 @@
 	.text
 
 
-	.global greenControl
+	.global greenControln	;allows this variable to be use everywhere
 	.extern P1OUT
 
 greenControl:
-	cmp #0, r12
-	jz off
+	cmp #0, r12		;compare destication and sourse
+	jz off			;jmp to off , 2^6=64, green== bit 6
 	bis #64, &P1OUT		;or
 	pop r0
 off:	and #~64, &P1OUT
